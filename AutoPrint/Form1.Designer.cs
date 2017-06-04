7b01e767d1c02fde5e2052b7c4c6564f78132ad5
Peng Lv <owdely@vip.163.com>
2017-06-04 10:17:57 +0800

    添加项目文件。

---
+++ AutoPrint/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AutoPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Lockall = new System.Windows.Forms.Button();
+            this.tim_Unlock = new System.Windows.Forms.Timer(this.components);
+            this.btn_printURL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Lockall
+            // 
+            this.btn_Lockall.Location = new System.Drawing.Point(110, 407);
+            this.btn_Lockall.Name = "btn_Lockall";
+            this.btn_Lockall.Size = new System.Drawing.Size(75, 23);
+            this.btn_Lockall.TabIndex = 0;
+            this.btn_Lockall.Text = "Lock";
+            this.btn_Lockall.UseVisualStyleBackColor = true;
+            this.btn_Lockall.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tim_Unlock
+            // 
+            this.tim_Unlock.Interval = 10000;
+            this.tim_Unlock.Tick += new System.EventHandler(this.tim_Unlock_Tick);
+            // 
+            // btn_printURL
+            // 
+            this.btn_printURL.Location = new System.Drawing.Point(348, 407);
+            this.btn_printURL.Name = "btn_printURL";
+            this.btn_printURL.Size = new System.Drawing.Size(75, 23);
+            this.btn_printURL.TabIndex = 1;
+            this.btn_printURL.Text = "PrintALL";
+            this.btn_printURL.UseVisualStyleBackColor = true;
+            this.btn_printURL.Click += new System.EventHandler(this.btn_printURL_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 583);
+            this.Controls.Add(this.btn_printURL);
+            this.Controls.Add(this.btn_Lockall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Lockall;
+        private System.Windows.Forms.Timer tim_Unlock;
+        private System.Windows.Forms.Button btn_printURL;
+    }
+}
+
---
+++ AutoPrint/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+
+namespace AutoPrint
+{
+    public partial class Form1 : Form
+    {
+        //引入API函数
+        [DllImport("user32 ")]
+        public static extern bool LockWorkStation();//这个是调用windows的系统锁定
+        [DllImport("user32.dll")]
+        static extern void BlockInput(bool Block);
+        public static string strPrintURL = "file:///" + Directory.GetCurrentDirectory() + "/PrintTemp/print.html";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lockAll();
+            tim_Unlock.Enabled = true;
+        }
+        private void lockAll()
+        {
+            BlockInput(true);//锁定鼠标及键盘
+        }
+
+        private void tim_Unlock_Tick(object sender, EventArgs e)
+        {
+            BlockInput(false);
+        }
+
+        private void btn_printURL_Click(object sender, EventArgs e)
+        {
+            // Create a WebBrowser instance. 
+            WebBrowser webBrowserForPrinting = new WebBrowser();
+            // Add an event handler that prints the document after it loads.
+            webBrowserForPrinting.DocumentCompleted +=
+                new WebBrowserDocumentCompletedEventHandler(PrintDocument);
+            // Set the Url property to load the document.   
+            strPrintURL = "http://121.196.216.179:70/tlwms/api/print/psd_auto?print_num=1";
+            webBrowserForPrinting.Url = new Uri(strPrintURL);
+        }
+        private void PrintDocument(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            // Print the document now that it is fully loaded.
+            ((WebBrowser)sender).Document.Encoding = "UTF-8";
+            ((WebBrowser)sender).Print();
+            // Dispose the WebBrowser now that the task is complete. 
+            ((WebBrowser)sender).Dispose();
+        }
+    }
+}
